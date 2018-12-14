241169dc049d3198b4d7f8530bc8e32f5d7740a1
kwonohdong <odong831@gmail.com>
2018-12-14 22:55:07 +0900

    The presence of two kangaroos at the intersection.

---
+++ problem_solving/p14_kangaroo.py
@@ -0,0 +1,39 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the kangaroo function below.
+def kangaroo(x1, v1, x2, v2):
+    result = ''
+
+    if x1 < x2 and v1 <= v2:
+        result = 'NO'
+    elif (x2 - x1) % (v1 - v2) == 0:
+        result = 'YES'
+    else:
+        result = 'NO'
+        
+    return result
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    x1V1X2V2 = input().split()
+
+    x1 = int(x1V1X2V2[0])
+
+    v1 = int(x1V1X2V2[1])
+
+    x2 = int(x1V1X2V2[2])
+
+    v2 = int(x1V1X2V2[3])
+
+    result = kangaroo(x1, v1, x2, v2)
+
+    fptr.write(result + '\n')
+
+    fptr.close()
