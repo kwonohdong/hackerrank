2cf5f19f0da5ebe1d490aa24c223f928c4ad5048
kwonohdong <odong831@gmail.com>
2018-12-17 15:10:54 +0900

    Using instance variables

---
+++ 30days_of_code/day14_scope.py
@@ -0,0 +1,27 @@
+class Difference:
+    def __init__(self, a):
+        self.__elements = a
+
+    # maximumDifference = 0
+
+	# Add your code here
+    def computeDifference(self):
+        result = []
+
+        for i in self.__elements:
+            for j in self.__elements:
+                if abs(i - j) not in result:
+                    result.append(abs(i - j))
+        
+        Difference.maximumDifference = sorted(result, reverse=True)[0]
+
+# End of Difference class
+
+_ = input()
+a = [int(e) for e in input().split(' ')]
+# a = [8, 8, 8]
+
+d = Difference(a)
+d.computeDifference()
+
+print(d.maximumDifference)
